f8a7d3d5d48a5afd297dfa4b4094c6b96fc88472
DJ52005 <144876361+DJ52005@users.noreply.github.com>
2023-09-18 19:18:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+T = int(input("Enter the number of test cases:"))
+for i in range(T):
+    X = int(input("Enter the water consumed in ml:"))
+    if X >= 2000:
+        print("Yes")
+    else:
+        print("No")
